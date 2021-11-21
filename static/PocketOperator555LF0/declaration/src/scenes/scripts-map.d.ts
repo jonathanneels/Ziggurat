321b86f53fb256a3c0b41d320b3629f7661e71f5
jonathanneels <jonathanneels@hotmail.com>
2021-11-21 12:29:32 +0100

    Add files via upload

---
+++ static/PocketOperator555LF0/declaration/src/scenes/scripts-map.d.ts
@@ -0,0 +1,10 @@
+import { ScriptMap } from "./tools";
+/**
+ * Defines the interface that exposes all exported scripts in this project.
+ */
+export interface ISceneScriptMap extends ScriptMap {
+}
+/**
+ * Defines the map of all available scripts in the project.
+ */
+export declare const scriptsMap: ISceneScriptMap;
